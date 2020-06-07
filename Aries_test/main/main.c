5bdf3c51328d4b4623afec73edbc2aa6cc838805
Sai_Teja <vellankisaiteja114@gmail.com>
2020-06-07 14:13:58 +0530

    removed updowncounter changed to upcounter

--- Aries_test/main/main.c
+++ Aries_test/main/main.c
@@ -383,7 +383,7 @@ static void pwm_init(void)
     pwm_config.frequency = 50;    //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
     pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
     pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
-    pwm_config.counter_mode = MCPWM_UP_DOWN_COUNTER;
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
     pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
     mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);   
     mcpwm_init(MCPWM_UNIT_1, MCPWM_TIMER_0, &pwm_config); 
